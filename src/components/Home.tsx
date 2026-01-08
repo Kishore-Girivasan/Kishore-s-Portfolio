cb24237b328b1530de83245174c76c736091d272
kishore-girivasan <kishoregirivasan11@gmail.com>
2026-01-08 16:34:40 +0530

    add resume to download

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ function Home(){
             <h3>Hello, this is</h3>
             <h1>Kishore G.</h1>
             <p>A Full Stack Developer with experience in React, TypeScript, Ruby on Rails</p>
-            <button className="resume-btn">Resume</button>
+            <a href="/Kishore_G_Software_Developer.pdf" download="Kishore_G_Software_Developer"><button className="resume-btn">Resume</button></a>
         </div>
     );
 }
